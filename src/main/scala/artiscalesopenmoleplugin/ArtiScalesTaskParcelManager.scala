9df075df93c71b759b6d6ffb97b789e4c4b1c858
maxcolomb <colomb.maxime@gmail.com>
2019-02-20 19:03:52 +0100

    make the parcelManager really distribuable

--- src/main/scala/artiscalesopenmoleplugin/ArtiScalesTaskParcelManager.scala
+++ src/main/scala/artiscalesopenmoleplugin/ArtiScalesTaskParcelManager.scala
@@ -6,12 +6,12 @@ import fr.ign.cogit.modules.SelectParcels
 import fr.ign.cogit.simplu3d.util.SimpluParametersJSON
 
 trait ArtiScalesTaskParcelManager {
-  def apply(rootFolder: File, outputFolder: File, varianteSpatialConf: File, paramFile1: File, paramFile2: File): Unit = {
+  def apply(rootFolder: File, outputFolder: File, varianteSpatialConf: File, paramFile1: File, paramFile2: File, zip: String ): Unit = {
 	val lF = new java.util.ArrayList[File]
 	lF.add(paramFile1)
 	lF.add(paramFile2)
 	val p: SimpluParametersJSON = new SimpluParametersJSON(lF)
-	new SelectParcels(rootFolder, outputFolder, varianteSpatialConf, p).run
+	new SelectParcels(rootFolder, outputFolder, varianteSpatialConf, p).selectAndDecompParcels(zip, false, null)
   }
 }
 
