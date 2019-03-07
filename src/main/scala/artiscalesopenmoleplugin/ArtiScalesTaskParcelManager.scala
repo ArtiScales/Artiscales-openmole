9a3ef614cd63cc7c48a9381a8dfe48b2cf0b94ed
Julien Perret <julien.perret@gmail.com>
2019-03-08 00:17:51 +0100

    adding Try to catch errors in tasks (?)

--- src/main/scala/artiscalesopenmoleplugin/ArtiScalesTaskParcelManager.scala
+++ src/main/scala/artiscalesopenmoleplugin/ArtiScalesTaskParcelManager.scala
@@ -5,13 +5,20 @@ import java.io.File
 import fr.ign.cogit.modules.SelectParcels
 import fr.ign.cogit.simplu3d.util.SimpluParametersJSON
 
+import scala.util.{Failure, Success, Try}
+
 trait ArtiScalesTaskParcelManager {
   def apply(rootFolder: File, outputFolder: File, varianteSpatialConf: File, paramFile1: File, paramFile2: File, zip: String ): Unit = {
 		val lF = new java.util.ArrayList[File]
 		lF.add(paramFile1)
 		lF.add(paramFile2)
 		val p: SimpluParametersJSON = new SimpluParametersJSON(lF)
-		new SelectParcels(rootFolder, outputFolder, varianteSpatialConf, p).selectAndDecompParcels(zip, false, null)
+		Try(new SelectParcels(rootFolder, outputFolder, varianteSpatialConf, p).selectAndDecompParcels(zip, false, null)) match {
+			case Success(_) =>
+			case Failure(exception) =>
+				println(s"Failure with $exception")
+				println(s"""Output folder contains:\n${outputFolder.list().mkString(",")}""")
+		}
 	}
 }
 
--- src/main/scala/artiscalesopenmoleplugin/ArtiScalesTaskSimPLU.scala
+++ src/main/scala/artiscalesopenmoleplugin/ArtiScalesTaskSimPLU.scala
@@ -5,6 +5,8 @@ import java.io.File
 import fr.ign.cogit.modules.SimPLUSimulator
 import fr.ign.cogit.simplu3d.util.SimpluParametersJSON
 
+import scala.util.{Failure, Success, Try}
+
 trait ArtiScalesTaskSimPLU {
   def apply(rootFolder: File, inputFolder: File, outputFolder: File, paramFile1: File, paramFile2: File): Unit = {
     val lF = new java.util.ArrayList[File]
@@ -12,7 +14,12 @@ trait ArtiScalesTaskSimPLU {
     lF.add(paramFile2)
     val p: SimpluParametersJSON = new SimpluParametersJSON(lF)
     val simulator = new SimPLUSimulator(rootFolder, inputFolder, p, outputFolder)
-    simulator.run
+    Try(simulator.run) match {
+      case Success(_) =>
+      case Failure(exception) =>
+        println(s"Failure with $exception")
+        println(s"""Output folder contains:\n${outputFolder.list().mkString(",")}""")
+    }
   }
 }
 
